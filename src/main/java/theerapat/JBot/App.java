4379541686e3437cff09c9f836aab52e51808421
Theerapat30 <t.komeinek@gmail.com>
2017-03-23 23:17:29 +0700

    delete code back to default

--- src/main/java/theerapat/JBot/App.java
+++ src/main/java/theerapat/JBot/App.java
@@ -28,15 +28,7 @@ public class App
     
     @EventMapping
     public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) throws IOException {
-    	String accessToken = "Bra2+2U13hEbgOlU6lOa1+z1ZIThZzJ0Fuyv9/IzcE0EEyfHMK1CrBfxbNOIBVXn0hJkuZtFRAEChRtgePSW4PzOL7Gg3i3CHlqRbYPLALxTod7j7CWssmbDWFTeq3BS7Ar3sAi38rO2sqMZN4fPjgdB04t89/1O/w1cDnyilFU=";
-    	TextMessage textMessage = new TextMessage("Hello");
-    	ReplyMessage replyMessage = new ReplyMessage(event.getReplyToken(), textMessage);
-    	Response<BotApiResponse> response = LineMessagingServiceBuilder
-    	.create(accessToken)
-    	.build()
-    	.replyMessage(replyMessage)
-    	.execute();
-        System.out.println(response.code() + " " + response.message());
+    	System.out.println("event: " + event);
         return new TextMessage(event.getMessage().getText());
     }
 
