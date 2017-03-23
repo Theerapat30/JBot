79dbc7c9e91294283c7584b15282b4560fa264ba
theerapat <theerapat@isc-intl.com>
2017-03-23 17:56:12 +0700

    Add test function

--- src/main/java/theerapat/JBot/App.java
+++ src/main/java/theerapat/JBot/App.java
@@ -1,13 +1,46 @@
 package theerapat.JBot;
 
-/**
- * Hello world!
- *
- */
+import java.io.IOException;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.linecorp.bot.client.LineMessagingServiceBuilder;
+import com.linecorp.bot.model.ReplyMessage;
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.model.response.BotApiResponse;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+
+import retrofit2.Response;
+
+@SpringBootApplication
+@LineMessageHandler
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	SpringApplication.run(App.class, args);
+    }
+    
+    @EventMapping
+    public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) throws IOException {
+    	TextMessage textMessage = new TextMessage("Hello");
+    	ReplyMessage replyMessage = new ReplyMessage("", textMessage);
+    	Response<BotApiResponse> response = LineMessagingServiceBuilder
+    	.create("")
+    	.build()
+    	.replyMessage(replyMessage)
+    	.execute();
+        System.out.println(response.code() + " " + response.message());
+        return new TextMessage(event.getMessage().getText());
+    }
+
+    @EventMapping
+    public void handleDefaultMessageEvent(Event event) {
+        System.out.println("event: " + event);
     }
 }
